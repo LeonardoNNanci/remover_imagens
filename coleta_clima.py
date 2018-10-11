72d2bf695cf132dde66ac69cfb0e185b35d2fc5c
LeonardoNNanci <leonardonanci@id.uff.br>
2018-10-11 20:28:00 -0300

    Inicio Multiplataforma

--- coleta_clima.py
+++ coleta_clima.py
@@ -3,24 +3,43 @@ import os
 from metar import Metar
 
 
+def comando_linux(data_hora):
+    comando = "wget --user-agent=\"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)\" --base=\"http://www.redemet.aer.mil.br/api/consulta_automatica/index.php\" --referer=\"http://www.redemet.aer.mil.br/api/consulta_automatica/index.php\" --post-data=\"&local=sbrj&msg=metar&data_ini=" + \
+        data_hora + "&data_fim=" + data_hora + \
+        "\" -O resultado.txt http://www.redemet.aer.mil.br/api/consulta_automatica/index.php"
+    os.system(comando)
+
+
+def comando_windows(data_hora):
+    pass
+
+
+def __init__():
+    global comando
+    if os.uname().sysname == 'Linux':
+        comando = comando_linux
+    elif os.name.sysname == "Win32":
+        comando = comando_windows
+        pass
+
+
 def coleta(data_hora):
     try:
-        os.system(
-            "wget --user-agent=\"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)\" --base=\"http://www.redemet.aer.mil.br/api/consulta_automatica/index.php\" --referer=\"http://www.redemet.aer.mil.br/api/consulta_automatica/index.php\" --post-data=\"&local=sbrj&msg=metar&data_ini=" + data_hora + "&data_fim=" + data_hora + "\" -O resultado.txt http://www.redemet.aer.mil.br/api/consulta_automatica/index.php")
+        comando(data_hora)
         d = {}
         linha = open("resultado.txt", 'r').readline()
         traduzido = Metar.Metar(linha[13:-2])
 
-        vento = traduzido.wind()
-        vento_dir = str(traduzido.wind_dir)
-        vento_vel = vento.split(" ")[2]
+        # vento = traduzido.wind()
+        # vento_dir = str(traduzido.wind_dir)
+        # vento_vel = vento.split(" ")[2]
 
         nuvens = traduzido.sky_conditions()
         visibilidade = traduzido.visibility()
         temperatura = str(traduzido.temp)
         pressao = str(traduzido.press)
 
-        d['vento'] = [vento_dir, vento_vel]
+        # d['vento'] = [vento_dir, vento_vel]
         d['nuvens'] = nuvens
         d['visibilidade'] = visibilidade
         d['temperatura'] = temperatura
@@ -28,5 +47,6 @@ def coleta(data_hora):
 
         return d
 
-    except IOError:
+    except IOError as e:
+        print(e)
         return 0
--- remove_imagens.py
+++ remove_imagens.py
@@ -6,39 +6,63 @@ import coleta_clima
 import exifread
 
 
-def mover_imagem(orig, dest):
-    dest = dest + orig.split("/")[-2]
-    if not os.path.exists(dest):
-        os.makedirs(dest)
-    shutil.copy2(orig, dest + "/")
+def __init__():
+    if os.uname().sysname == 'Linux':
+        busca = "/home/leo/Documentos/CC_UFF/IC/Imagens/"
+        destino = "/home/leo/Documentos/CC_UFF/IC/cp/"
+    elif os.uname().sysname == 'Win32':
+        busca = ""
+        detino = ""
+
+    verifica_pastas(busca, destino)
+    coleta_clima.__init__()
+
+    return map(str, [busca, destino])
 
 
-destino = "/home/leo/Documentos/CC UFF/Iniciação Científica/cp/"
-diretorios = glob.iglob(
-    "/home/leo/Documentos/CC UFF/Iniciação Científica/Imagens/*/*.jpg")
+def verifica_pastas(busca, destino):
+    paths = glob.iglob(busca + "*/")
+    for path in paths:
+        pasta = path[-8:]
+        if not os.path.exists(destino + pasta):
+            os.makedirs(destino + pasta)
 
-for origem in diretorios:
-    try:
-        img = open(origem, "rb")
-        data_hora = exifread.process_file(img, stop_tag='EXIF DateTimeOriginal')[
-            'EXIF DateTimeOriginal']
-        data_hora = str(data_hora)
-        img.close()
 
-        hora = data_hora[11:13]
-        hora = int(hora)
+def seleciona_imagens():
+    busca, destino = __init__()
 
-        # Reorganiza a string apara o formato YYYYMMDDHH
-        data_hora = data_hora[:4] + data_hora[5:7] + \
-            data_hora[8:10] + data_hora[11:13]
+    arqs = glob.iglob(busca + "*/*.jpg")
 
-        visibilidade = coleta_clima.coleta(data_hora)['visibilidade']
-        visibilidade = visibilidade.split()[-2]
-        visibilidade = int(visibilidade)
+    for origem in arqs:
+        try:
+            img = open(origem, "rb")
+            data_hora = exifread.process_file(
+                img, stop_tag='EXIF DateTimeOriginal')
+            data_hora = data_hora['EXIF DateTimeOriginal']
+            data_hora = str(data_hora)
+            img.close()
 
-        # Setar melhor horários & visibilidade
-        if((6 <= hora < 19) and (visibilidade > 5000)):
-            mover_imagem(origem, destino)
+            hora = data_hora[11:13]
+            hora = int(hora)
 
-    except Exception as e:
-        print(e)
+            # Reorganiza a string apara o formato YYYYMMDDHH
+            data_hora = data_hora[:4] + data_hora[5:7] + \
+                data_hora[8:10] + data_hora[11:13]
+
+            visibilidade = coleta_clima.coleta(data_hora)
+            visibilidade = visibilidade['visibilidade']
+            visibilidade = visibilidade.split()[-2]
+            visibilidade = int(visibilidade)
+
+            # Setar melhor horarios & visibilidade
+            if((6 <= hora < 19) and (visibilidade > 5000)):
+                pasta = origem.split("/")[-2] + "/"
+                shutil.copy2(origem, destino + pasta)
+
+        except Exception as e:
+            print(e)
+            exit()
+
+
+if __name__ == '__main__':
+    seleciona_imagens()
